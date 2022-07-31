a5905e77e659abb6c7dba5743d8b5210e134403b
Moritz Schramm <moritz.schramm@gmx.de>
2022-07-31 20:48:21 +0200

    Support ormconfig.json

--- src/code/app.js
+++ src/code/app.js
@@ -1,6 +1,42 @@
 import ts from 'typescript';
 
 export function generateAppModule(entities) {
+    const pathCommonImport = ts.factory.createImportDeclaration(
+        undefined,
+        undefined,
+        ts.factory.createObjectLiteralExpression(
+            [
+                ts.factory.createIdentifier('resolve')
+            ]
+        ),
+        ts.factory.createStringLiteral('path', true)
+    );
+
+    const ormConfig = ts.factory.createVariableStatement(
+        [ts.factory.createModifier(ts.SyntaxKind.ConstKeyword)],
+        ts.factory.createVariableDeclaration(
+            'ormconfig',
+            undefined,
+            undefined,
+            ts.factory.createCallExpression(
+                ts.factory.createIdentifier('require'),
+                undefined,
+                [
+                    ts.factory.createCallExpression(
+                        ts.factory.createIdentifier('resolve'),
+                        undefined,
+                        [
+                            ts.factory.createCallExpression(
+                                ts.factory.createPropertyAccessExpression(ts.factory.createIdentifier('process'), 'cwd')
+                            ),
+                            ts.factory.createStringLiteral('ormconfig.json', true)
+                        ]
+                    )
+                ]
+            )
+        )
+    );
+
     const nestjsCommonImport = ts.factory.createImportDeclaration(
         undefined,
         undefined,
@@ -29,7 +65,21 @@ export function generateAppModule(entities) {
             [ts.factory.createIdentifier(`${entity.name}Module`)]
         ),
         ts.factory.createStringLiteral(`../module/${entity.name}Module`, true)
-    ))
+    ));
+
+    const entitiesLiterals = entities.map((entity) => ts.factory.createIdentifier(entity.name));
+    const entityImports = entities.map((entity) => ts.factory.createImportDeclaration(
+        undefined,
+        undefined,
+        ts.factory.createObjectLiteralExpression(
+            [ts.factory.createIdentifier(`${entity.name}`)]
+        ),
+        ts.factory.createStringLiteral(`../entity/${entity.name}`, true)
+    ));
+    const config = ts.factory.createObjectLiteralExpression([
+        ts.factory.createPropertyAssignment('entities', ts.factory.createArrayLiteralExpression(entitiesLiterals)),
+        ts.factory.createSpreadAssignment(ts.factory.createIdentifier('ormconfig'))
+    ]);
 
     const moduleAnnotation = ts.factory.createDecorator(
         ts.factory.createCallExpression(ts.factory.createIdentifier('Module'), undefined, [
@@ -38,7 +88,7 @@ export function generateAppModule(entities) {
                     ts.factory.createCallExpression(
                         ts.factory.createPropertyAccessExpression(ts.factory.createIdentifier('TypeOrmModule'), 'forRoot'),
                         undefined,
-                        []
+                        [config]
                     ),
                     ...modulesLiterals
                 ]))
@@ -57,7 +107,7 @@ export function generateAppModule(entities) {
 
     return {
         name: 'AppModule',
-        node: ts.factory.createSourceFile([nestjsCommonImport, nestjsTypeORMImport, ...moduleImports, classNode]),
+        node: ts.factory.createSourceFile([pathCommonImport, nestjsCommonImport, nestjsTypeORMImport, ...moduleImports, ...entityImports, ormConfig, classNode]),
         type: 'app'
     };
 }
