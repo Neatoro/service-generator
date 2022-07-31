5748c149de8aeeda895f0a9eae599299e4ebec59
Moritz Schramm <moritz.schramm@gmx.de>
2022-07-31 17:14:28 +0200

    Add id column to every entity

--- src/code/index.js
+++ src/code/index.js
@@ -19,7 +19,7 @@ function generateEntity(entity) {
         undefined,
         undefined,
         ts.factory.createObjectLiteralExpression(
-            [ts.factory.createIdentifier('Entity'), ts.factory.createIdentifier('Column')]
+            [ts.factory.createIdentifier('Entity'), ts.factory.createIdentifier('Column'), ts.factory.createIdentifier('PrimaryGeneratedColumn')]
         ),
         ts.factory.createIdentifier('typeorm')
     );
@@ -28,15 +28,19 @@ function generateEntity(entity) {
         ts.factory.createCallExpression(ts.factory.createIdentifier('Entity'))
     );
 
+    const idAnnotation = ts.factory.createDecorator(
+        ts.factory.createCallExpression(ts.factory.createIdentifier('PrimaryGeneratedColumn'), undefined, [ts.factory.createStringLiteral('uuid', true)])
+    );
+    const idProperty = ts.factory.createPropertyDeclaration([idAnnotation], undefined, 'id', undefined, ts.factory.createTypeReferenceNode('string'), undefined);
+
     const properties = entity.properties.map((property) => {
         const columnAnnotation = ts.factory.createDecorator(
             ts.factory.createCallExpression(ts.factory.createIdentifier('Column'))
         );
 
-        return ts.factory.createPropertyDeclaration([columnAnnotation], undefined, property.name, undefined, ts.factory.createTypeReferenceNode(property.type), undefined);
-    });
+        return ts.factory.createPropertyDeclaration([columnAnnotation], undefined, property.name, undefined, ts.factory.createTypeReferenceNode(property.type), undefined);    });
 
-    const classNode = ts.factory.createClassDeclaration([entityAnnotation], undefined, entity.name, undefined, [], properties);
+    const classNode = ts.factory.createClassDeclaration([entityAnnotation], undefined, entity.name, undefined, [], [idProperty, ...properties]);
 
     const block = ts.factory.createSourceFile([typeOrmImport, classNode]);
     return {
